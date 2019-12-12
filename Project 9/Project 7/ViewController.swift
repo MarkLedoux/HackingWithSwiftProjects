36c1a7d15eb545507c488f29d73ec052a0f85449
vinceled <mark.ledoux@live.fr>
2019-12-13 00:28:10 +0100

    Day 39 of 100 Days of Swift

---
+++ Project 9/Project 7/ViewController.swift
@@ -0,0 +1,109 @@
+//
+//  ViewController.swift
+//  Project 7
+//
+//  Created by Mark LEDOUX  on 09/12/2019.
+//  Copyright © 2019 Mark LEDOUX . All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UITableViewController {
+    var petitions = [Petition]()
+    var storedPetition = [Petition]()
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+        
+        navigationItem.rightBarButtonItem = UIBarButtonItem(barButtonSystemItem: .action, target: self, action: #selector(showInfo))
+        navigationItem.leftBarButtonItem = UIBarButtonItem(barButtonSystemItem: .action, target: self, action: #selector(filterPetition))
+
+        performSelector(inBackground: #selector(fetchJSON), with: nil)
+        
+    }
+    
+   @objc func fetchJSON() {
+        let urlString: String
+            
+        if navigationController?.tabBarItem.tag  == 0 {
+            urlString = "https://www.hackingwithswift.com/samples/petitions-1.json"
+        } else {
+            urlString = "https://www.hackingwithswift.com/samples/petitions-2.json"
+        }
+        
+            if let url = URL(string: urlString) {
+                if let data = try? Data(contentsOf: url) {
+                    parse(json: data)
+                    return
+                }
+            }
+    performSelector(onMainThread: #selector(showError), with: nil, waitUntilDone: false)
+    }
+    
+   @objc func showError() {
+            let ac = UIAlertController(title: "Loading error", message: "There was an error loading the feed, please check your connection and try again.", preferredStyle: .alert)
+            ac.addAction(UIAlertAction(title: "OK", style: .default))
+            present(ac, animated: true)
+    }
+    
+    func parse(json: Data) {
+        let decoder = JSONDecoder()
+        
+        if let jsonPetitions  = try? decoder.decode(Petitions.self, from: json) {
+            petitions = jsonPetitions.results
+            tableView.performSelector(onMainThread: #selector(UITableView.reloadData), with: nil, waitUntilDone: false)
+        } else {
+            performSelector(onMainThread: #selector(showError), with: nil, waitUntilDone: false)
+        }
+    }
+    
+    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return petitions.count
+    }
+    
+    override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "Cell", for: indexPath)
+        
+        let petition = petitions[indexPath.row]
+        cell.textLabel?.text = petition.title
+        cell.detailTextLabel?.text = petition.body
+        
+        return cell
+    }
+    
+    override func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        let vc = DetailViewController()
+        vc.detailItem = petitions[indexPath.row]
+        navigationController?.pushViewController(vc, animated: true)
+    }
+    
+    @objc func showInfo() {
+         let ac = UIAlertController(title: "Info", message: "The data shown in this application comes from the We The People API of the Whitehouse.", preferredStyle: .alert)
+        
+        let action = UIAlertAction(title: "OK", style: .default)
+        
+        ac.addAction(action)
+        present(ac, animated: true)
+    }
+    
+    @objc func filterPetition() {
+        let ac = UIAlertController(title: "Enter filtering word", message: nil, preferredStyle: .alert)
+        ac.addTextField()
+        
+        let submitAction = UIAlertAction(title: "Submit", style: .default) {
+            [weak self, weak ac] action in
+            guard let answer = ac?.textFields?[0].text else { return }
+            self?.submit(answer)
+        }
+        ac.addAction(submitAction)
+        present(ac, animated: true)
+    }
+    
+    func submit(_ answer: String) {
+        _ = answer.lowercased()
+    }
+
+
+}
+

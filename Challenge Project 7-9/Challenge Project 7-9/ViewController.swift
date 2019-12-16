f1ec47c3d42e866fc7736b4e7e4bce3000c921c2
vinceled <mark.ledoux@live.fr>
2019-12-16 12:15:47 +0100

    Creating the project for the challenge of day 41 of 100 days of swift

---
+++ Challenge Project 7-9/Challenge Project 7-9/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Challenge Project 7-9
+//
+//  Created by Mark LEDOUX  on 16/12/2019.
+//  Copyright © 2019 Mark LEDOUX . All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+

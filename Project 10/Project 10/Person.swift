3b6eb6f5fa9078ad36b672764993934b8d9caf72
vinceled <mark.ledoux@live.fr>
2019-12-17 22:24:42 +0100

    day 43 of 100 days of swift

---
+++ Project 10/Project 10/Person.swift
@@ -0,0 +1,20 @@
+//
+//  Person.swift
+//  Project 10
+//
+//  Created by Mark LEDOUX  on 17/12/2019.
+//  Copyright © 2019 Mark LEDOUX . All rights reserved.
+//
+
+import UIKit
+
+class Person: NSObject {
+    var name: String
+    var image: String
+    
+    init(name: String, image: String) {
+        self.name = name
+        self.image = image
+    }
+
+}

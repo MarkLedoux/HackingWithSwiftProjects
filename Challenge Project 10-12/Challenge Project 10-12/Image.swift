218e271df207cf9d33cd25ac7261fd170d52635a
vinceled <mark.ledoux@live.fr>
2019-12-30 15:42:42 +0100

    day 53 of 100 days of swift

---
+++ Challenge Project 10-12/Challenge Project 10-12/Image.swift
@@ -0,0 +1,19 @@
+//
+//  Image.swift
+//  Challenge Project 10-12
+//
+//  Created by Mark LEDOUX  on 29/12/2019.
+//  Copyright © 2019 Mark LEDOUX . All rights reserved.
+//
+
+import UIKit
+
+class Image: NSObject {
+    var filename: String
+    var caption: String
+    
+    init(filename: String, caption: String) {
+        self.filename = filename
+        self.caption = caption
+    }
+}

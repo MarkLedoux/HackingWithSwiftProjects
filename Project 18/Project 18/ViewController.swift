2e506a24c3a903aeb980e20ef293df9c4625d156
vinceled <mark.ledoux@live.fr>
2020-01-27 09:12:20 +0100

    day 64 of 100 days of swift

---
+++ Project 18/Project 18/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Project 18
+//
+//  Created by Mark LEDOUX  on 24/01/2020.
+//  Copyright © 2020 Mark LEDOUX . All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        for i in 1...100 {
+            print("Got number \(i).")
+        }
+    }
+}
+

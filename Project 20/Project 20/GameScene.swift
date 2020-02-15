9e3050bf63ee632b7e254d750557730279c7f0c1
vinceled <mark.ledoux@live.fr>
2020-02-15 11:16:25 +0100

    day 70 of 100 days of swift part 1-1

---
+++ Project 20/Project 20/GameScene.swift
@@ -0,0 +1,16 @@
+//
+//  GameScene.swift
+//  Project 20
+//
+//  Created by Mark LEDOUX  on 15/02/2020.
+//  Copyright © 2020 Mark LEDOUX . All rights reserved.
+//
+
+import SpriteKit
+
+class GameScene: SKScene {
+    
+    override func didMove(to view: SKView) {
+        
+    }
+}

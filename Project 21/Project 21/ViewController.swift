b8fec2a97bc90a4f0c434b290dbce25ea1a9a19e
vinceled <mark.ledoux@live.fr>
2020-02-29 14:54:01 +0100

    setting up for project 21 of 100 days of swift, day 72 part 1

---
+++ Project 21/Project 21/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Project 21
+//
+//  Created by Mark LEDOUX  on 29/02/2020.
+//  Copyright © 2020 Mark LEDOUX . All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+

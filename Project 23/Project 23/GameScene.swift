b48530e2a9d39aae6eb083861d71eb70c0a70d02
vinceled <mark.ledoux@live.fr>
2020-03-03 16:37:56 +0100

    day 77 part 2 of 100 days of swift

--- Project 23/Project 23/GameScene.swift
+++ Project 23/Project 23/GameScene.swift
@@ -19,6 +19,9 @@ class GameScene: SKScene {
     var liveImages = [SKSpriteNode]()
     var lives = 3
     
+    var activeSliceBG: SKShapeNode!
+    var activeSliceFG: SKShapeNode!
+    
     override func didMove(to view: SKView) {
         let background = SKSpriteNode(imageNamed: "sliceBackground")
         background.position = CGPoint(x: 512, y: 384)
@@ -35,14 +38,38 @@ class GameScene: SKScene {
     }
     
     func createScore() {
+        gameScore = SKLabelNode(fontNamed: "Chalkduster")
+        gameScore.horizontalAlignmentMode = .left
+        gameScore.fontSize = 48
+        addChild(gameScore)
         
+        gameScore.position = CGPoint(x: 8, y: 8)
+        score = 0
     }
     
     func createLives() {
-        
+        for i in 0..<3 {
+            let spriteNode = SKSpriteNode(imageNamed: "sliceLife")
+            spriteNode.position = CGPoint(x: CGFloat(834 + (i * 70)), y: 720)
+            addChild(spriteNode)
+            liveImages.append(spriteNode)
+        }
     }
     
     func createSlices() {
+        activeSliceBG = SKShapeNode()
+        activeSliceBG.zPosition = 2
         
+        activeSliceFG = SKShapeNode()
+        activeSliceFG.zPosition = 3
+        
+        activeSliceBG.strokeColor = UIColor(red: 1, green: 0.9, blue: 0, alpha: 1)
+        activeSliceBG.lineWidth = 9
+        
+        activeSliceFG.strokeColor = UIColor.white
+        activeSliceFG.lineWidth = 5
+        
+        addChild(activeSliceBG)
+        addChild(activeSliceFG)
     }
 }

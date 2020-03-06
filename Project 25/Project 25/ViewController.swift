511e2f873223a53db722773e52af8aef5aa0ee50
vinceled <mark.ledoux@live.fr>
2020-03-06 09:17:26 +0100

    setting up project 25 for 100 days of hacking with swift

---
+++ Project 25/Project 25/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Project 25
+//
+//  Created by Mark LEDOUX  on 06/03/2020.
+//  Copyright © 2020 Mark LEDOUX . All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UICollectionViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        title = "Selfie Share"
+    }
+
+
+}
+

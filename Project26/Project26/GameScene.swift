b45f02bd3f988db5abe404ff234408a649b12912
vinceled <mark.ledoux@live.fr>
2020-03-07 18:20:25 +0100

    setting up project 26 of 100 day of swift on day 85

---
+++ Project26/Project26/GameScene.swift
@@ -0,0 +1,16 @@
+//
+//  GameScene.swift
+//  Project26
+//
+//  Created by Mark LEDOUX  on 07/03/2020.
+//  Copyright © 2020 Mark LEDOUX . All rights reserved.
+//
+
+import SpriteKit
+
+class GameScene: SKScene {
+    
+    override func didMove(to view: SKView) {
+
+    }
+}
